9bf1b8d305b610a4c7d04168675a6f79a6c12fa9
JeremyPPG <muhammadrizqi2303@yahoo.com>
2023-05-09 09:02:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -17,14 +17,24 @@ let ctx = canvas.getContext('2d');
 
 
 function init() {
+	var headSize = 8;
+    var bodyHeight = 12;
+    var defaultLimbWidth = 4;
 //con.drawImage(img, sx, sy, sw, sh, x, y, w, h)
   ctx.drawImage(img2, 0, 0, 64, 64, 0, 0, 64, 64);
   //head
-  ctx.drawImage(img, 8, 8, 8, 8, 20, 20, 8, 8);//front
-  ctx.drawImage(img, 16, 8, 8, 8, 28, 20, 4, 8);//left
-  ctx.drawImage(img, 0, 8, 8, 8, 16, 20, 4, 8);//right
-  ctx.drawImage(img, 8, 0, 8, 8, 20, 16, 8, 4);//top
-  ctx.drawImage(img, 24, 8, 8, 8, 32, 20, 8, 8);//back
+  ctx.drawImage(img, 8, 8, headSize, headSize, 20, 20, 8, 8);//front
+  ctx.drawImage(img, 16, 8, headSize, headSize, 28, 20, 4, 8);//left
+  ctx.drawImage(img, 0, 8, headSize, headSize, 16, 20, 4, 8);//right
+  ctx.drawImage(img, 8, 0, headSize, headSize, 20, 16, 8, 4);//top
+  ctx.drawImage(img, 24, 8, headSize, headSize, 32, 20, 8, 8);//back
+  
+  //head2
+  ctx.drawImage(img, 40, 8, headSize, headSize, 20, 36, 8, 8);//front
+  ctx.drawImage(img, 48, 8, headSize, headSize, 28, 36, 4, 8);//left
+  ctx.drawImage(img, 32, 8, headSize, headSize, 16, 36, 4, 8);//right
+  ctx.drawImage(img, 40, 0, headSize, headSize, 20, 32, 8, 4);//top
+  ctx.drawImage(img, 56, 8, headSize, headSize, 32, 36, 8, 8);//back
   
   //body
   ctx.drawImage(img, 16, 20, 24, 4, 16, 28, 24, 4);//front
@@ -38,5 +48,5 @@ ctx.mozImageSmoothingEnabled = false;
 ctx.webkitImageSmoothingEnabled = false;
 ctx.imageSmoothingEnabled = false;
   
-  ctx.drawImage(img, 15, 15, 16, 18, 0, 0, 16, 18);
+  //ctx.drawImage(img, 15, 15, 16, 18, 0, 0, 16, 18);
 }
