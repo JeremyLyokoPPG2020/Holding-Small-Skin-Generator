84d5cca3c67017f2b24bb5f38511a824db75f63f
JeremyPPG <muhammadrizqi2303@yahoo.com>
2023-10-03 07:27:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let imgskin1 = new Image();
-imgskin1.src = 'img/skin-template.png';
+imgskin1.src = '';
 imgskin1.onload = function() {
   //init();
 };
@@ -29,19 +29,23 @@ imgskin2.onload = function(){
 	
 	//body
     var bodyHeight = 12;
-	var bodyHeig1 = 4;
-	var bodyHeig2 = 8;
+	//var bodyHeig1 = 5;
+	//var bodyHeig2 = 7;
+	
+	var bodyHeight1 = bodyHeight - 7; //5px
+	var bodyHeight2 = bodyHeight - 5; //7px
 	
 	//limb
     var defaultLimbWidth = 4;
     var defaultLimbHeight = 12;
 	
 	//resize body
-	var bodyResiz1 = 7; //+4
+	var bodyResiz1 = 4; //+4
 	var bodyResiz2 = 5; //-8
 	
 	//resize leg
 	var legResiz = 12;
+	var legHeigh = 7;
 	
 	//con.drawImage(img, sx, sy, sw, sh, x, y, w, h)
   //head
@@ -63,13 +67,38 @@ imgskin2.onload = function(){
   //canvas.height = img.height;
   
   //ctx.drawImage(imgskin2, 16, 20, 24, 4, 16, 28, 24, bodyHeig1);
-  ctx.drawImage(imgskin2, 16, 20, 24, bodyResiz1, 16, 28, 24, bodyHeig1);//front
+  ctx.drawImage(imgskin2, 16, 20, 24, bodyHeight1, 16, 28, 24, bodyResiz1);//front
   //ctx.drawImage(imgskin2, 16, 24, 12, bodyHeig2, 0, 20, 12, 8);//resize
-  ctx.drawImage(imgskin2, 16, 24, 12, bodyHeig2, 0, 20, 12, bodyResiz2);//resize
-  ctx.drawImage(imgskin2, 20, 24, 16, bodyHeig2, 16, 52, 16, bodyResiz2);//resize
-  ctx.drawImage(imgskin2, 36, 24, 4, bodyHeig2, 12, 20, 4, bodyResiz2);//resize
+  ctx.drawImage(imgskin2, 16, 25, 12, bodyHeight2, 0, 20, 12, bodyResiz2);//resize
+  ctx.drawImage(imgskin2, 20, 25, 16, bodyHeight2, 16, 52, 16, bodyResiz2);//resize
+  ctx.drawImage(imgskin2, 36, 25, 4, bodyHeight2, 12, 20, 4, bodyResiz2);//resize
+  
   
   //leg
+  ctx.drawImage(imgskin2, 0, 20, 16, defaultLimbHeight, 0, 25, 16, legHeigh);
+  ctx.drawImage(imgskin2, 16, 52, 16, defaultLimbHeight, 16, 57, 16, legHeigh);
+  
+  //arm
+  // Translate the canvas to the image's center.
+	// Rotate the canvas by 180 degrees clockwise.
+	ctx.rotate(Math.PI);
+  //ctx.drawImage(imgskin2, 0, 0, 64, 64, 0, 0, 64, 64);
+  ctx.drawImage(imgskin2, 44, 20, defaultLimbWidth, defaultLimbHeight, -48, -32, defaultLimbWidth, defaultLimbHeight);
+  ctx.drawImage(imgskin2, 48, 20, defaultLimbWidth, defaultLimbHeight, -44, -32, defaultLimbWidth, defaultLimbHeight);
+  ctx.drawImage(imgskin2, 40, 20, defaultLimbWidth, defaultLimbHeight, -52, -32, defaultLimbWidth, defaultLimbHeight);
+  ctx.drawImage(imgskin2, 52, 20, defaultLimbWidth, defaultLimbHeight, -56, -32, defaultLimbWidth, defaultLimbHeight);
+  
+  ctx.drawImage(imgskin2, 48, 16, defaultLimbWidth, defaultLimbWidth, -48, -20, defaultLimbWidth, defaultLimbWidth);
+  ctx.drawImage(imgskin2, 44, 16, defaultLimbWidth, defaultLimbWidth, -52, -20, defaultLimbWidth, defaultLimbWidth);
+  
+  ctx.translate(0, 11);
+  ctx.drawImage(imgskin2, 36, 52, defaultLimbWidth, defaultLimbHeight, -40, -75, defaultLimbWidth, defaultLimbHeight);
+  ctx.drawImage(imgskin2, 40, 52, defaultLimbWidth, defaultLimbHeight, -36, -75, defaultLimbWidth, defaultLimbHeight);
+  ctx.drawImage(imgskin2, 32, 52, defaultLimbWidth, defaultLimbHeight, -44, -75, defaultLimbWidth, defaultLimbHeight);
+  ctx.drawImage(imgskin2, 44, 52, defaultLimbWidth, defaultLimbHeight, -48, -75, defaultLimbWidth, defaultLimbHeight);
+  
+  ctx.drawImage(imgskin2, 40, 48, defaultLimbWidth, defaultLimbWidth, -40, -63, defaultLimbWidth, defaultLimbWidth);
+  ctx.drawImage(imgskin2, 36, 48, defaultLimbWidth, defaultLimbWidth, -44, -63, defaultLimbWidth, defaultLimbWidth);
   
   
   //ctx.msImageSmoothingEnabled = false;
